a35343280d5160bb3eaab1dd166fe2c123e00787
Sourav Aggarwal <souravagg01@gmail.com>
2022-12-30 12:30:39 +0530

    30-12-2022

---
+++ Extras/Single valued subtree.cpp
@@ -0,0 +1,45 @@
+/* Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};*/
+
+class Solution
+{
+    public:
+    int cnt = 0;
+    bool isLeaf(Node *root) {
+        return (root && !root->left && !root->right);
+    }
+    
+    bool isSingleValued(Node *root) {
+        if(root == NULL) {
+            return true;    
+        }
+        
+        bool left = isSingleValued(root->left), right = isSingleValued(root->right);
+        
+        if(!left || !right) return false;
+        
+        if(root->left && root->data != root->left->data) {
+            return false;
+        }
+        
+        if(root->right && root->data != root->right->data) {
+            return false;
+        }
+        
+        cnt++;
+        return true;
+        
+    }
+    
+    int singlevalued(Node *root)
+    {
+        bool t = isSingleValued(root);
+        return cnt;
+    }
+    
+};
