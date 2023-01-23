50ac32ae826bd24192f523dde9f7b884f5d025b5
Sourav Aggarwal <souravagg01@gmail.com>
2023-01-23 17:51:35 +0530

    23-01-2023

--- Extras/Geek and the String.cpp
+++ Extras/Geek and the String.cpp
@@ -1,35 +1,34 @@
 class Solution
 {
 public:
+    void helper(string &s, int idx)
+    {
+        if (s.length() <= 0 || idx >= s.length() - 1)
+            return;
+
+        if (s[idx] == s[idx + 1])
+        {
+            s = s.substr(0, idx) + s.substr(idx + 2);
+
+            if (idx > 0)
+                helper(s, idx - 1);
+            else
+                helper(s, idx);
+        }
+        else
+            helper(s, idx + 1);
+    }
+
     string removePair(string s)
     {
         // code here
-        string ans = "";
-        stack<char> st;
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (st.empty())
-                st.push(s[i]);
-            else
-            {
-                if (st.top() == s[i])
-                    st.pop();
-                else
-                    st.push(s[i]);
-            }
-        }
-
-        while (!st.empty())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
-
-        if (ans == "")
+        if (s.length() == 0)
+            return "-1";
+        if (s.length() == 2 && s[0] == s[1])
             return "-1";
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        helper(s, 0);
+
+        return s == "" ? "-1" : s;
     }
 };
